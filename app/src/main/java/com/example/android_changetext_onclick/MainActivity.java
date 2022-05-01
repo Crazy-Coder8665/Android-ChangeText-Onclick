5a19b37538441d73de76b6887509acf472126c7f
Talha Masood <talha8665i@gmail.com>
2022-05-01 14:23:00 +0500

    Initial commit

---
+++ app/src/main/java/com/example/android_changetext_onclick/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android_changetext_onclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.textView);
+    }
+
+    public void changeText(View view) {
+        txt.setText("Welcome!!!");
+    }
+}
